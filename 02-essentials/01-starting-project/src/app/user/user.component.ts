b061729725860eb0a751f00dc5437fa955abb44c
SSDAsim <asim.saeed@halimsol.com>
2025-11-07 10:01:57 +0500

    Required and Optional Components

--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -8,10 +8,12 @@ import { Component, Input} from '@angular/core';
 })
 export class UserComponent {
 
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // the '!' with property name is to tell typescript that we are going to set the value of this property from outside and that we know we have not initialized this property with any value.
+  // the '!' with property name is to tell typescript that we are essentially going to set the value of this property from outside (which is not always the case) and that we know we have not initialized this property with any value. 
+  // If you do not pass any value to the property in the markup, there will be error but it wall cause issues in the UI. so, you have to mark properties as required so it shows error in the code and does not execute code.
+  // To mark a property as required, pass configuration option {required: true} to the @Input decorator
 
   // image path getter
   get imagePath() {
