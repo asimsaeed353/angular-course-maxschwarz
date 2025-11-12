be2b6fb020917fed39e92a4ff73a695b6212542c
asimsaeed <asimsaeed353@gmail.com>
2025-11-12 17:04:35 +0500

    146. Two-Way Binding: Repetition

--- 06-components-and-templates - deep dive/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-and-templates - deep dive/01-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -20,9 +20,18 @@ export class NewTicketComponent {
   // @Output() add = new EventEmitter<{title: string; text: string}>();
   add = output<{title: string; text: string}>();
 
+  // get the input data using the Two-Way Binding
+  enteredTitle = '';
+  enteredText = '';
+
    // capute the form input value(s) using Template Variables
   onSubmit(titleElement: string, textElement: string) {
-    this.add.emit({title: titleElement, text: textElement});
-    this.form?.nativeElement.reset();
+    // this.add.emit({title: titleElement, text: textElement});
+    this.add.emit({title: this.enteredTitle, text: this.enteredText});
+
+    // this.form?.nativeElement.reset();
+    /* Alternatively, since we are using two-way binding you can reset the form by writing empty string to the input fields */
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
