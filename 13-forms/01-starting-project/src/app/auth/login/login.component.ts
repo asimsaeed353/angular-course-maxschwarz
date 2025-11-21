3e4b33125c7bea58193cd8063b463ce47c8d65c2
asimsaeed <asimsaeed353@gmail.com>
2025-11-21 14:42:11 +0500

    247. Interacting With The Underlying Form Object In The Component

--- 13-forms/01-starting-project/src/app/auth/login/login.component.ts
+++ 13-forms/01-starting-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,25 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private destroyRef = inject(DestroyRef);
+
+  // in order to store the value of the email so user does not has to enter the email again 
+  // get hold of the form element 
+  private form = viewChild.required<NgForm>('form');
+
+  /* the form will be rendered after the component has fully initialized, */
+  constructor() {
+    /* Register a callback to be invoked the next time the application finishes rendering */
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+      });
+      /* this observable will emit new value everytime the value of the form changes (i.e. with every key stroke) */
+      /* debounceTime(500) keep the value only if there's a break of 500ms */
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
   onSubmitForm(formData: NgForm){
 
     if(formData.form.invalid){
@@ -18,5 +38,7 @@ export class LoginComponent {
     const email = formData.form.value.email;
     const password = formData.form.value.password;
     console.log(formData);
+
+    formData.form.reset();
   }
 }
