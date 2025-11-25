b280bbadc9f3ba3bbd83e3c3470764b4c2cb52d2
asimsaeed <asimsaeed353@gmail.com>
2025-11-25 11:07:10 +0500

    282. Splitting Route Definitions Across Multiple Files

--- 14-routing/01-starting-project/src/app/app.routes.ts
+++ 14-routing/01-starting-project/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from "@angular/router";
+import { routes as userRoutes } from "./users/user.routes"; 
 import { TasksComponent } from "./tasks/tasks.component";
-import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [    
@@ -13,21 +12,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId', //<domain>/users/<uid>
         component: UserTasksComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'tasks',
-                pathMatch: 'full',
-            },
-            {
-                path: 'tasks', //<domain>/users/<uid>/tasks
-                component: TasksComponent,
-            },
-            {
-                path: 'tasks/new', //<domain>/users/<uid>/tasks/new
-                component: NewTaskComponent,
-            },
-        ]
+        children: userRoutes,
     },
     {
         path: '**',
