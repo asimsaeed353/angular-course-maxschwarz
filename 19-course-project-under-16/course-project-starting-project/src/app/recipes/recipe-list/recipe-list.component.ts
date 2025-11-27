16db711821c19e3c120db1e70e3f0be6b2132cb5
asimsaeed <asimsaeed353@gmail.com>
2025-11-27 12:01:19 +0500

    374. Adding Content to the Recipes Components

--- 19-course-project-under-16/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 19-course-project-under-16/course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test recipe', 'https://thomascattlecompany.com/cdn/shop/articles/20240408152714-img_8556.jpg?v=1729021399&width=1100')
+  ];
 }
